40a578d56312909f4ef81c3fd87657e0c8c3ca8d
victorien_bonard <victorien_bonard@ens.univ-artois.fr>
2023-01-29 15:29:48 +0100

    Trying to add code coverage

--- src/main/java/validator/EmailValidator.java
+++ src/main/java/validator/EmailValidator.java
@@ -3,7 +3,7 @@ package validator;
 import java.util.regex.Pattern;
 
 public class EmailValidator {
-    private static final String EMAIL_PATTERN = "^[A-Z0-9._+%&*-]+(?:\\.[A-Z0-9_+&*-]+)*@[A-Z0-9.-]+\\.[A-Z]{2,6}$";
+    private static final String EMAIL_PATTERN = "^[A-Z0-9._+%&*-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$";
 
     private EmailValidator() {
         throw new IllegalStateException("Utility class");
